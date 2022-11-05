60495f3d395c63fa7fc892a831bfd7d46cc87981
Igor Parchinski <parczynski@gmail.com>
2022-11-06 00:52:47 +0400

    Add implementation for telegram

--- src/handlers/telegram.ts
+++ src/handlers/telegram.ts
@@ -1,8 +1,46 @@
+import axios from 'axios'
 import { Estate } from '../classes/estate'
 import { Handler } from '../classes/handler'
 
 export class Telegram extends Handler {
-	handle( estate: Estate ): Promise<void> {
-		throw new Error( 'Method not implemented.' )
+
+	protected chats: string[] = []
+
+	constructor ( protected token: string ) {
+		super()
 	}
+
+	addChat( chat: string ) {
+		this.chats.push( chat )
+	}
+
+	prepareMessage( estate: Estate ): string {
+
+		const image = ( estate.images.length > 0 )
+			? `[image](${estate.images[0]})`
+			: ''
+		
+
+		return `*№* ${estate.id}%0A` +
+			`${estate.link}%0A` +
+			`${estate.address}%0A` +
+			`*Price:* ${estate.price}%0A` +
+			`*Floor:* ${estate.floor}%0A` +
+			`*Rooms:* ${estate.bedrooms} / ${estate.rooms}%0A` +
+			`*Area:* ${estate.area}%0A` +
+			`${image}`
+
+	}
+
+	async handle( estate: Estate ): Promise<void> {
+
+		const message = this.prepareMessage( estate )
+
+		for await ( const chat of this.chats ) {
+			await axios.get( `https://api.telegram.org/bot${this.token}/sendMessage?parse_mode=Markdown&text=${message}&chat_id=${chat}` )
+		}
+
+	}
+
+
 }
